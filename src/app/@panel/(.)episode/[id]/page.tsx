4add06f3ec91545e7e6d218d0ce8608afa05a40e
Yi Fan <yifan.9916@gmail.com>
2023-08-25 17:37:11 +0200

    finish basic layout and responsiveness

--- src/app/@panel/(.)episode/[id]/page.tsx
+++ src/app/@panel/(.)episode/[id]/page.tsx
@@ -15,28 +15,30 @@ export default async function Page(props: Props) {
   const episode = Omdb.processEpisode(omdbEpisode);
 
   return (
-    <div className="h-screen bg-white">
-      <Image
-        src={episode.img}
-        alt={episode.plot}
-        height={1920}
-        width={1080}
-        className="max-h-[66vh] w-full object-cover"
-      />
+    <div className="absolute z-10 h-screen w-screen p-4 bg-[rgba(0,0,0,0.7)] md:relative md:p-0 md:bg-none md:basis-1/3 md:grow-0 md:shrink-0">
+      <div className="rounded overflow-hidden bg-white h-full w-full">
+        <Image
+          src={episode.img}
+          alt={episode.plot}
+          height={1920}
+          width={1080}
+          className="max-h-[66vh] w-full object-cover"
+        />
 
-      <div>
-        <div className="flex justify-between border-b p-10">
-          <span>
-            Episode {episode.episode} - {episode.releaseDate}
-          </span>{' '}
-          <span>
-            <b>{episode.rating}</b>/10
-          </span>
-        </div>
+        <div>
+          <div className="flex justify-between border-b p-10">
+            <span>
+              Episode {episode.episode} - {episode.releaseDate}
+            </span>{' '}
+            <span>
+              <b>{episode.rating}</b>/10
+            </span>
+          </div>
 
-        <div className="p-10">
-          <h1 className="mb-2 text-2xl font-bold">{episode.title}</h1>
-          <p>{episode.plot}</p>
+          <div className="p-10">
+            <h1 className="mb-2 text-2xl font-bold">{episode.title}</h1>
+            <p>{episode.plot}</p>
+          </div>
         </div>
       </div>
     </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,9 +20,9 @@ export default function RootLayout(props: Props) {
 
   return (
     <html lang="en">
-      <body className={inter.className}>
-        {panel}
+      <body className={`${inter.className} flex`}>
         {children}
+        {panel}
       </body>
     </html>
   );
